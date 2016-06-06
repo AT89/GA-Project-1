11af381cec35aa8b18b56d4df09e422aff1401e4
Anh Truong <anh.truong.07@cnu.edu>
2016-06-06 14:14:44 -0400

    working on draw function

--- projects/project1/war.js
+++ projects/project1/war.js
@@ -7,7 +7,8 @@ var p2DeadPile;
 var deck = [];
 assignValue(); //first assign values and images
 shuffle(deck); //shuffle the deck (create button later)
-initialDeal();
+initialDeal(); //deals 26 to each player
+
 
 function assignValue(){ // assign value and images to cards (while still ordered)
             var value = 0;
@@ -37,7 +38,6 @@ function shuffle(array) { //credits to Fisher Yates shuffle https://bost.ocks.or
     array[m] = array[i];
     array[i] = t;
   }
-
   return array;
 }
 
@@ -48,19 +48,32 @@ function initialDeal(){
     return [p1Deck, p2Deck];
 }
 
-function deadPileDeal(){
-    //deal the deadpile into hand
-}
-
 function draw(){
     // when player1 clicks on his deck, the top card from both decks will draw
+    while (p1Deck.length > 0 || p2Deck.length > 0){ //WRONG SYNTAX
+        var p1Active = p1Deck[0];
+        var p2Active = p2Deck[0];
+        return p1Active, p2Active;
+    }
 }
 
 function battle(){
     // will evaluate whoevers drawn card is higher and take both cards and put it in the winner's deadpile
+    if (p1Deck.value[0] === p2deck.value[0]){
+        tieBreaker();
+    }
+    if (p1Deck.value[0] > p2deck.value[0]){ //p1 wins
+
+    }
+    else if (p1Deck.value[0] < p2deck.value[0]){ //p2 wins
+    }
+}
+
+function deadPileDeal(){
+    //deal the deadpile into hand
 }
 
 function tieBreaker(){
     // occurs when the value of the cards are the same, both players draw 3 cards and then flips the 4th one over and that is the playing cards
     //function can repeat itself if the 4th card is a draw
-};
+}
